cbae9de47163e40ac769ecf8ddeda6b035f51ebd
baorejason <15990572573@163.com>
2018-06-30 18:39:13 +0800

    1

--- src/testSSH/TestSSH.java
+++ src/testSSH/TestSSH.java
@@ -4,7 +4,7 @@ public class TestSSH {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("怎1么提交到1 gitHub?");
+		System.out.println("怎1么提交到1 gitHub?怎么进行版本比对");
 	}
 
 }
