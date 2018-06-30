519fa892d9cbd8914cfa7a9809bad5244053a74e
baorejason <15990572573@163.com>
2018-06-30 18:51:47 +0800

    提交到哪里呢?本地仓库还是远程仓库

--- src/testSSH/TestSSH.java
+++ src/testSSH/TestSSH.java
@@ -4,7 +4,9 @@ public class TestSSH {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("怎1么提交到1 11gitHub?怎么进行版本比对");
+		System.out.println("怎么提交到gitHub?");
+		System.out.println("怎么进行版本比对");
+		System.out.println("怎么删除github仓库");
 	}
 
 }
