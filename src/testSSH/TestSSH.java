388014c8c8b415d467a787f30746fa8a644dfc81
baorejason <15990572573@163.com>
2019-12-21 18:30:12 +0800

    直接提交

--- src/testSSH/TestSSH.java
+++ src/testSSH/TestSSH.java
@@ -3,10 +3,19 @@ package testSSH;
 public class TestSSH {
 
 	public static void main(String[] args) {
+		//提交到本地仓库 commit
 		System.out.println("Hello-Git");
+		//提交到远程仓库 ||同步进行 push ||commit and push
 		System.out.println("Hello-git - commit");
-		//you更新哦;
-		System.out.println("hi 冲突了.");
+		//更新 pull;
+		System.out.println("git update");
+		//拉取 冲突,怎么办哪?
+		System.out.println("拉取更新");
+		//解决冲突
+		System.out.println("");
+		//add index
+		System.out.println("");
+		//commit
 	}
 
 }
