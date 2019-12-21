8e66eb796237cb18ee1c7c127dbd679fbb7cd154
baorejason <15990572573@163.com>
2019-12-21 11:19:33 +0800

    to local rep

--- src/testSSH/TestSSH.java
+++ src/testSSH/TestSSH.java
@@ -3,10 +3,8 @@ package testSSH;
 public class TestSSH {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("怎么提交到gitHub?");
-		System.out.println("怎么进行版本比对");
-		System.out.println("怎么删除github仓库");
+		System.out.println("Hello-Git");
+		
 	}
 
 }
